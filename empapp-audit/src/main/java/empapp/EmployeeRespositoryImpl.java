3d7616dfc73b0093804aa64d58677e52b321e13d
vicziani <viczian.istvan@gmail.com>
2019-10-15 13:42:27 +0200

    Convert DTO

--- empapp-audit/src/main/java/empapp/EmployeeRespositoryImpl.java
+++ empapp-audit/src/main/java/empapp/EmployeeRespositoryImpl.java
@@ -1,11 +1,17 @@
 package empapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.envers.AuditReader;
 import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionType;
 import org.hibernate.envers.query.AuditEntity;
+import org.modelmapper.ModelMapper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 public class EmployeeRespositoryImpl implements EmployeeRepositoryCustom {
@@ -21,5 +27,18 @@ public class EmployeeRespositoryImpl implements EmployeeRepositoryCustom {
                 .add(AuditEntity.id().eq(id)).getResultList();
 
 
+    }
+
+    private EmployeeHistoryDto toHistoryDto(Object[] resultItem) {
+        var modelMapper = new ModelMapper();
+        var employee = (Employee) resultItem[0];
+        var dto =  modelMapper.map(employee, EmployeeHistoryDto.class);
+        var revision = (DefaultRevisionEntity) resultItem[1];
+        dto.setRevNumber(revision.getId());
+        dto.setModifiedAt(LocalDateTime.ofInstant(revision.getRevisionDate().toInstant(),
+                ZoneId.systemDefault()));
+        var type = (RevisionType) resultItem[2];
+        dto.setType(type.toString());
+        return dto;
     }
 }
